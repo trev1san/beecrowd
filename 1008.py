89c67565eb3e50fbce99aa8cec4902d1db6fb7c5
Danilo R. Trevisan <mamacohomi@gmai.com>
2024-10-01 19:23:26 -0300

    first commit

---
+++ 1008.py
@@ -0,0 +1,6 @@
+NUMBER = float(input())
+FUNCIONARIOS = float(input())
+OUTRO = float(input())
+SALARY = OUTRO * FUNCIONARIOS
+print(f"NUMBER = {NUMBER:.0f}")
+print(f"SALARY = U$ {SALARY:.2f}")
---
+++ 1012.py
@@ -0,0 +1,19 @@
+A,B,C = input().split()
+A = float (A)
+B = float (B)
+C = float (C)
+TRIANGULO = (A * C) /2
+
+print(f"TRIANGULO: {TRIANGULO:.3f}")
+pi = 3.14159
+CIRCULO = (pi * C **2)
+print(f"CIRCULO: {CIRCULO:.3f}")
+
+TRAPEZIO = ((A + B) * C) /2
+print(f"TRAPEZIO: {TRAPEZIO:.3f}")
+
+QUADRADO = (B * B)
+print(f"QUADRADO: {QUADRADO:.3f}")
+
+RETANGULO = (A * B)
+print(f"RETANGULO: {RETANGULO:.3f}")
