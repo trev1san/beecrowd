5752c3a5294337bd7bb274bae9e1103061c23a44
Danilo R. Trevisan <mamacohomi@gmai.com>
2024-10-01 19:26:12 -0300

    alteracao do 1015.py

--- 1015.py
+++ 1015.py
@@ -6,4 +6,3 @@ o = math.sqrt((x2 - x1 ) ** 2 + (y2 - y1) **2)
 print(f"{o:.4f}")  
 
 
-#se usa map por causa q ja fica tudo na mesma linha, sem precisar usa outras linhas no code, o float pode ser trocado por int numa boa.
