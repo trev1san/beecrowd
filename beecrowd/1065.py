c3c2151507ca9fd53ade2b43b723896b06890fd9
Danilo Ramos Trevisan <mamacohomi@gmail.com>
2026-03-17 22:46:00 -0300

    1065.py

--- beecrowd/1065.py
+++ beecrowd/1065.py
@@ -1,6 +1,6 @@
 a, b, c, d, e = int(input()), int(input()), int(input()), int(input()), int(input())
 positivos = 0
 for n in [a, b, c, d, e]:
-    if n > 0:
+    if n % 2 == 0:
         positivos += 1
 print(f"{positivos} valores positivos")
